44d439117e09b64cd0f518d897c77aef85e2dbc7
alice02 <mk.alice02@gmail.com>
2018-07-09 01:36:41 +0900

    Fix to use unofficial golang sdk for nifcloud (github.com/alice02/nifcloud-sdk-go)

--- lib/nifcloud-rdb.go
+++ lib/nifcloud-rdb.go
@@ -4,12 +4,19 @@ import (
 	"errors"
 	"flag"
 	"log"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/alice02/nifcloud-sdk-go/nifcloud"
+	"github.com/alice02/nifcloud-sdk-go/nifcloud/credentials"
+	"github.com/alice02/nifcloud-sdk-go/nifcloud/session"
+	"github.com/alice02/nifcloud-sdk-go/service/rdb"
 	mp "github.com/mackerelio/go-mackerel-plugin"
 )
+
+const layout = "2006-01-02T15:04:05Z"
 
 // RDBPlugin mackerel plugin for NIFCLOUD RDB
 type RDBPlugin struct {
@@ -22,40 +29,38 @@ type RDBPlugin struct {
 	LabelPrefix     string
 }
 
-func getLastPoint(client *RdbClient, identifier, metricName string) (float64, error) {
+func getLastPoint(client *rdb.Rdb, dimension *rdb.RequestDimensionsStruct, metricName string) (float64, error) {
 	now := time.Now().In(time.UTC)
-	const layout = "2006-01-02 15:04:05"
-	params := make(map[string]string)
-	params["Dimensions.member.1.Name"] = "DBInstanceIdentifier"
-	params["Dimensions.member.1.Value"] = identifier
-	params["EndTime"] = now.Format(layout)
-	params["StartTime"] = now.Add(time.Duration(180) * time.Second * -1).Format(layout) // 3 min (to fetch at least 1 data-point)
-	params["MetricName"] = metricName
 
-	response, err := client.NiftyGetMetricStatistics(params)
+	response, err := client.NiftyGetMetricStatistics(&rdb.NiftyGetMetricStatisticsInput{
+		Dimensions: []*rdb.RequestDimensionsStruct{dimension},
+		StartTime:  nifcloud.Time(now.Add(time.Duration(180) * time.Second * -1)), // 3 min (to fetch at least 1 data-point)
+		EndTime:    nifcloud.Time(now),
+		MetricName: nifcloud.String(metricName),
+	})
 	if err != nil {
 		return 0, err
 	}
 
-	datapoints := response.NiftyGetMetricStatisticsResult.Datapoints
+	datapoints := response.Datapoints
 	if len(datapoints) == 0 {
 		return 0, errors.New("fetched no datapoints")
-	}
-	members := datapoints[0].Member
-	if len(members) == 0 {
-		return 0, errors.New("fetched no members")
 	}
 
 	latest := new(time.Time)
 	var latestVal float64
-	for _, m := range members {
-		if m.Timestamp.Before(*latest) {
+	for _, dp := range datapoints {
+		ts, _ := time.Parse(layout, *dp.Timestamp)
+		if ts.Before(*latest) {
 			continue
 		}
 
-		latest = &m.Timestamp
-		latestVal = float64(m.Sum) / float64(m.SampleCount)
+		latest = &ts
+		sum, _ := strconv.ParseFloat(*dp.Sum, 64)
+		count, _ := strconv.ParseFloat(*dp.SampleCount, 64)
+		latestVal = sum / count
 	}
+
 	return latestVal, nil
 }
 
@@ -70,15 +75,34 @@ func (p RDBPlugin) rdbMetrics() (metrics []string) {
 
 // FetchMetrics interface for mackerel-plugin
 func (p RDBPlugin) FetchMetrics() (map[string]float64, error) {
-	client := NewRdbClient(p.Region, p.AccessKeyID, p.SecretAccessKey)
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	config := nifcloud.NewConfig()
+	if p.AccessKeyID != "" && p.SecretAccessKey != "" {
+		config = config.WithCredentials(credentials.NewStaticCredentials(p.AccessKeyID, p.SecretAccessKey, ""))
+	}
+	if p.Region != "" {
+		config = config.WithRegion(p.Region)
+	}
+
+	client := rdb.New(sess, config)
 
 	stat := make(map[string]float64)
+
+	perInstance := &rdb.RequestDimensionsStruct{
+		Name:  nifcloud.String("DBInstanceIdentifier"),
+		Value: nifcloud.String(p.Identifier),
+	}
+
 	var wg sync.WaitGroup
 	for _, met := range p.rdbMetrics() {
 		wg.Add(1)
 		go func(met string) {
 			defer wg.Done()
-			v, err := getLastPoint(client, p.Identifier, met)
+			v, err := getLastPoint(client, perInstance, met)
 			if err == nil {
 				stat[met] = v
 			} else {
